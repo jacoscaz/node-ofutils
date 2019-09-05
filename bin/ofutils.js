54aef25e32130645dde68f2cf86a70b3250c4fc5
Jacopo Scazzosi <me@jacoscaz.com>
2019-09-05 13:43:03 +0200

    bumps version up to 0.0.1-alpha.10

--- bin/ofutils.js
+++ bin/ofutils.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 
+const package = require('../package.json');
 const program = require('commander');
 
 program
-  .version('0.0.1-alpha.9')
-  .command('report <report_type>', 'generate reports');
+  .version(package.version)
+  .command('report <report_type>', 'generate reports')
+;
 
 program.parse(process.argv);
